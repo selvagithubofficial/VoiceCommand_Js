f6a269c393830860af86693dc13ddf96657d6b2b
selvakumar <74043547+selvagithubofficial@users.noreply.github.com>
2020-11-15 14:42:27 +0530

    updation

--- js/script.js
+++ js/script.js
@@ -33,75 +33,81 @@ function speak_out_to_User(msg) {
   //speech_synthesis.speak(utterance);
 }
 function dark_light_mode(msg) {
-  if(msg.includes("dark")){
-    utterance.text=msg+"Mode enabled ";
+  let Lmsg=msg.toLowerCase();
+  if(Lmsg.includes("dark")){
     body.classList.add("dark");
-    para[0].innerHTML=utterance.text=msg+" enabled";
+    para[0].innerHTML=utterance.text="Dark mode enabled";
     speech_synthesis.speak(utterance);
     console.log(speech_synthesis);
   }
-  else if(msg.includes("light"))
+  else if(Lmsg.includes("light"))
   {
-  para[0].innerHTML=utterance.text=msg+"Mode enabled ";
+  para[0].innerHTML=utterance.text=Lmsg+"Light mode enabled ";
   body.classList.remove("dark");
   speech_synthesis.speak(utterance);
   console.dir(speech_synthesis);
   }
-  else if(msg.includes("who are you"))
+  else if(Lmsg.includes("who are you"))
   {
   para[0].innerHTML=utterance.text="i am javascript artifiial intelligent . My name is vivo v11. how can i help you?";
   speech_synthesis.speak(utterance);
   }
-  else if(msg.includes("YouTube"))
+  else if(Lmsg.includes("YouTube"))
   {
   para[0].innerHTML=utterance.text="youtube opening";
   speech_synthesis.speak(utterance);
   window.open("https://www.youtube.com/");
   }
-  else if(msg.includes("open Gmail"))
+  else if(Lmsg.includes("open Gmail"))
   {
   para[0].innerHTML=utterance.text="Your gmail account is opening";
   speech_synthesis.speak(utterance);
   window.open("https://mail.google.com/mail/u/0/");
   }
-  else if(msg.includes("open Twitter"))
+  else if(Lmsg.includes("open Twitter"))
   {
   para[0].innerHTML=utterance.text="Twitter opening";
   speech_synthesis.speak(utterance);
   window.open("https://twitter.com/explore");
   }
-  else if(msg.includes("story"))
+  else if(Lmsg.includes("story"))
   {
   para[0].innerHTML=utterance.text="A lion was once sleeping in the jungle when a mouse started running up and down his body just for fun. This disturbed the lion’s sleep, and he woke up quite angry. He was about to eat the mouse when the mouse desperately requested the lion to set him free. “/I promise you, I will be of great help to you someday if you save me.”/ The lion laughed at the  mouse’s confidence and let him go. One day, a few hunters came into the forest and took the lion with them. They tied him up against a tree. The lion was struggling to get out and started to whimper. Soon, the mouse walked past and noticed the lion in trouble. Quickly, he ran and gnawed on the ropes to set the lion free. Both of them sped off into the jungle. Moral of the Story. A small act of kindness can go a long way.";
   speech_synthesis.speak(utterance);
   }
-  else if(msg.includes("news"))
+  else if(Lmsg.includes("news"))
   {
   para[0].innerHTML=utterance.text="polimer news opening";
   speech_synthesis.speak(utterance);
   window.open("https://www.youtube.com/watch?v=Vq-9xWMiiVQ");
   }
-  else if(msg.includes("Amazon"))
+  else if(Lmsg.includes("Amazon"))
   {
   para[0].innerHTML=utterance.text="Amazon opening";
   speech_synthesis.speak(utterance);
   window.open("https://www.amazon.in/");
   }
-  else if(msg.includes("song") || msg.includes("favorite"))
+  else if(Lmsg.includes("song") || Lmsg.includes("favorite"))
   {
   para[0].innerHTML=utterance.text="Selva your favorite song is opening";
   speech_synthesis.speak(utterance);
   window.open("https://youtu.be/OjU54VhRFbU");
   }
-  else if(msg.includes("designer"))
+  else if(Lmsg.includes("designer"))
   {
   para[0].innerHTML=utterance.text="There are 4 members in designer team";
   speech_synthesis.speak(utterance);
   }
-  else if(msg.includes("developer"))
+  else if(Lmsg.includes("developer"))
   {
   para[0].innerHTML=utterance.text="There are 5 members in developer's team";
   speech_synthesis.speak(utterance);
+  }
+  else if(Lmsg.includes("wallpaper"))
+  {
+  para[0].innerHTML=utterance.text="Your Window background wallpaper  is changed";
+  speech_synthesis.speak(utterance);
+  body.classList.add("bodyimg");
   }
 }
 
